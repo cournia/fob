43511bfc979c44e31c4917b1c4b127d33f4a8657
Nathan Cournia <nathan@cournia.com>
2004-09-28 20:48:04 +0000

    - Fixed up the code for GCC 3.4.x. - Removed /usr/X11R6 link line in test Makefile. - Removed automake 1.6 dependency.

--- fob/fob.cpp
+++ fob/fob.cpp
@@ -28,6 +28,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //              master bird.
 
 #include <iostream>
+#include <cerrno>
 #include <iomanip>
 #include <cassert>
 #include <fcntl.h>
