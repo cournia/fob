06b1f00aa1ec1cdc6a88e7d181676501cd0abab3
Nathan Cournia <nathan@cournia.com>
2016-11-12 15:56:57 -0800

    Import to git.  New compiler fixes.

--- fob/fob.h
+++ fob/fob.h
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef COURNIA_FOB_H
 #define COURNIA_FOB_H 1
 
+#include <cstdio>
 #include <cstring>
 #include <vector>
 #include <string>
